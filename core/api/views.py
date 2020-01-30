ee564f2d6d228915297db94b1c6f12a0fa180230
Sandeep Hugar <sandeephugar19@gmail.com>
2020-01-30 19:10:46 +0530

    removed unnecessary import statements
    item, order and orderitem were twice imported from the core.models

--- core/api/views.py
+++ core/api/views.py
@@ -13,7 +13,6 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
-from core.models import Item, OrderItem, Order
 from .serializers import (
     ItemSerializer, OrderSerializer, ItemDetailSerializer, AddressSerializer,
     PaymentSerializer
